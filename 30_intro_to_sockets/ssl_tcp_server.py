3b5198d790f5d61c3954498378edcc40c178bd12
olekmali <olekmali@ieee.org>
2018-02-18 18:43:07 -0600

    Rearranged directories for this set of examples

---
+++ 30_intro_to_sockets/ssl_tcp_server.py
@@ -0,0 +1,28 @@
+import socket, ssl
+
+context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+context.load_cert_chain(certfile="mycertfile", keyfile="mykeyfile")
+
+bindsocket = socket.socket()
+bindsocket.bind(('myaddr.mydomain.com', 10023))
+bindsocket.listen(5)
+
+while True:
+    newsocket, fromaddr = bindsocket.accept()
+    connstream = context.wrap_socket(newsocket, server_side=True)
+    try:
+        deal_with_client(connstream)
+    finally:
+        connstream.shutdown(socket.SHUT_RDWR)
+        connstream.close()
+
+def deal_with_client(connstream):
+    data = connstream.read()
+    # null data means the client is finished with us
+    while data:
+        if not do_something(connstream, data):
+            # we'll assume do_something returns False
+            # when we're finished with client
+            break
+        data = connstream.read()
+    # finished with client
