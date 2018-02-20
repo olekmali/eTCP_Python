e13eb09d31acea28c435651b464e7d439388a08f
olekmali <olekmali@ieee.org>
2018-02-20 02:10:01 -0600

    SLL client example - tested SSL server example - problems with self signed certificate

--- 30_intro_to_sockets/331_ssl_tcp_server.py
+++ 30_intro_to_sockets/331_ssl_tcp_server.py
@@ -1,28 +1,40 @@
 import socket, ssl
 
-context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
-context.load_cert_chain(certfile="mycertfile", keyfile="mykeyfile")
+HOST, PORT = "localhost", 1201
 
-bindsocket = socket.socket()
-bindsocket.bind(('myaddr.mydomain.com', 1201))
-bindsocket.listen(1)
+count = 0
+
+
+def deal_with_client(request, client_address):
+    global count    # indicate that there is no new local variable called count
+    count = count + 1;
+
+    request.sendall( bytes( "Welcome to the server.\r\nWhat is your name please?\r\n", 'UTF-8' ) )
+    request.setblocking(True)
+    name = str( request.recv(1024), 'UTF-8' ).strip()
+    print( name , " has just contacted us from " , client_address )
+    data = "Nice to meet you " + name + ".\r\nYou are the " + str(count) + "th person who stopped by today\r\n"
+    request.sendall( bytes( data, 'UTF-8' ) )
+    # finished with client
+
+
+ssl_context = ssl.create_default_context(ssl.Purpose.CLIENT_AUTH)
+ssl_context.load_cert_chain(certfile="331_cert.pem", keyfile="331_cert.pem")
+""" The self-signed certificate was created by running:
+    openssl req -new -x509 -days 365 -nodes -out cert.pem -keyout cert.pem
+"""
+
+server_scoket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_scoket.bind( (HOST, PORT) )
+server_scoket.listen(1)
 
 while True:
-    newsocket, fromaddr = bindsocket.accept()
-    connstream = context.wrap_socket(newsocket, server_side=True)
+    client, fromaddr = server_scoket.accept()
+    ssl_client = ssl_context.wrap_socket(client, server_side=True)
     try:
-        deal_with_client(connstream)
+        deal_with_client(ssl_client, fromaddr)
+    except:
+        print( "Client connection error" )
     finally:
-        connstream.shutdown(socket.SHUT_RDWR)
-        connstream.close()
-
-def deal_with_client(connstream):
-    data = connstream.read()
-    # null data means the client is finished with us
-    while data:
-        if not do_something(connstream, data):
-            # we'll assume do_something returns False
-            # when we're finished with client
-            break
-        data = connstream.read()
-    # finished with client
+        ssl_client.shutdown(socket.SHUT_RDWR)
+        ssl_client.close()
--- 30_intro_to_sockets/332_ssl_tcp_client.py
+++ 30_intro_to_sockets/332_ssl_tcp_client.py
@@ -1,10 +1,34 @@
 import socket, ssl
 
-context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
-context.verify_mode = ssl.CERT_REQUIRED
-context.check_hostname = True
-context.load_default_certs()
+HOST, PORT = "localhost", 1201
+#HOST, PORT = "www.python.org", 443
 
-s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-ssl_sock = context.wrap_socket(s, server_hostname='www.verisign.com')
-ssl_sock.connect(('www.verisign.com', 443))
+ssl_context = ssl.create_default_context()
+ssl_context.check_hostname = True
+ssl_context.verify_mode = ssl.CERT_REQUIRED
+
+""" or try to do it your self from the scratch like below:
+    ssl_context = ssl.SSLContext(ssl.PROTOCOL_TLSv1)
+    ssl_context.verify_mode = ssl.CERT_REQUIRED # ssl.CERT_REQUIRED or ssl.CERT_OPTIONAL or ssl.CERT_NONE
+    ssl_context.check_hostname = True
+    ssl_context.load_default_certs()
+"""
+
+try:
+    client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    ssl_client = ssl_context.wrap_socket(client, server_hostname=HOST)
+    ssl_client.connect( (HOST, PORT) )
+    cert = ssl_client.getpeercert()
+    print( cert )
+    ###
+    data = ssl_client.recv(1024)
+    text = str( data, 'UTF-8' ).strip()
+    print( text )
+    ssl_client.sendall( bytes( "Automated Client\r\n", 'UTF-8' ))
+    data = ssl_client.recv(1024)
+    text = str( data, 'UTF-8' ).strip()
+    print( text )
+except:
+    print( "Socket connection error!" )
+finally:
+    ssl_client.close()
