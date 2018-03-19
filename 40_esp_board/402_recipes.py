dfeca3adaf850fe73f25b31612fd6c047b378437
olekmali <olekmali@ieee.org>
2018-03-19 00:58:01 -0500

    uC Python Web server example now handles multiple files

--- 40_esp_board/402_recipes.py
+++ 40_esp_board/402_recipes.py
@@ -9,16 +9,18 @@ test.myfn()
 from importlib import reload
 reload(test)
 
-
 # uPython: reload the module after the file contents were changed
-from machine import reset
-reset()
-# wait for the prompt and then
-import test
-test.myfn()
-
-
 # uPython: soft reset
 from machine import reset
 reset()
 
+# uPython: reload the module after the file contents were changed
+# uPython: partial reset (does not work well with WebREPL)
+Ctrl+C  # stops the current program
+Ctrl+D  # performs a soft reset
+
+
+# uPython: copy-paste program code into the terminal
+Ctrl+E
+<paste code>
+Ctrl+D
--- 50_esp_networked/501_web_server.py
+++ 50_esp_networked/501_web_server.py
@@ -1,19 +1,10 @@
 """ Simple HTTP server -- MicroPython Documentation """
 
 def httpd_pins():
+    header = "HTTP/1.1 %s\r\nServer: uPython on ESP8266\r\nConnection: close\r\nExpires: 0\r\nContent-Type: %s\r\n\r\n"
+
     import machine
     pins = [machine.Pin(i, machine.Pin.IN) for i in (0, 2, 4, 5, 12, 13, 14, 15)]
-
-    html_header = "HTTP/1.0 200 OK\r\nServer: uPython on ESP8266\r\nConnection: close\r\nExpires: 0\r\nContent-Type: text/html; charset=UTF-8\r\n\r\n"
-    png_header  = "HTTP/1.0 200 OK\r\nServer: uPython on ESP8266\r\nConnection: close\r\nExpires: 0\r\nContent-Type: image/png\r\n\r\n"
-    html_file1 = """<!DOCTYPE html>
-    <html>
-        <head> <title>ESP8266 Pins</title> </head>
-        <body> <h1>ESP8266 Pins</h1>
-            <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
-        </body>
-    </html>
-    """
 
     import socket
     addr = socket.getaddrinfo('0.0.0.0', 80)[0][-1]
@@ -23,7 +14,6 @@ def httpd_pins():
     s.listen(1)
 
     print('listening on', addr)
-
     while True:
         cl, addr = s.accept()
         print('client connected from', addr)
@@ -32,14 +22,35 @@ def httpd_pins():
         file    = request.split(' ')[1]
         # print('request line', request)
         print('request file', file)
-        while True:
-            line = cl_file.readline()
-            if not line or line == b'\r\n':
-                break
-        rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
-        response = html_file1 % '\n'.join(rows)
-        cl.send( bytes( html_header, 'UTF-8' ))
-        cl.send( bytes( response, 'UTF-8' ))
+
+        if file == '/' or file == '/index.html':
+            html_file1 = """<!DOCTYPE html>
+<html>
+    <head> <title>ESP8266 Pins</title> </head>
+    <body> <h1>ESP8266 Pins</h1>
+        <table border="1"> <tr><th>Pin</th><th>Value</th></tr> %s </table>
+    </body>
+</html>
+"""
+            while True:
+                line = cl_file.readline()
+                if not line or line == b'\r\n':
+                    break
+            rows = ['<tr><td>%s</td><td>%d</td></tr>' % (str(p), p.value()) for p in pins]
+            response = html_file1 % '\n'.join(rows)
+            cl.send( bytes( ( header % ('200 OK','text/html; charset=UTF-8') ), 'UTF-8' ))
+            cl.send( bytes( response, 'UTF-8' ))
+        else:
+            html_file2 = """<!DOCTYPE html>
+<html>
+    <head> <title>404 Not Found</title> </head>
+    <body> <h1>Resource not found</h1>
+        <p>Why don't you try <a href="/">here</a>?</p>
+    </body>
+</html>
+"""
+            cl.send( bytes( ( header % ('404 Not Found','text/html; charset=UTF-8') ), 'UTF-8' ))
+            cl.send( bytes( html_file2, 'UTF-8' ))
         cl.close()
         print('served')
 
