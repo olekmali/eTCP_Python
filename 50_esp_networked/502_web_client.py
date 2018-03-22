382c3d470c0988320e5ac548eb9492ca5ff0515e
olekmali <olekmali@ieee.org>
2018-03-22 02:04:03 -0500

    Correcxted wget for https and tested OK

--- 50_esp_networked/502_web_client.py
+++ 50_esp_networked/502_web_client.py
@@ -1,14 +1,14 @@
 """ Simple HTTP client -- MicroPython Documentation """
 
 def http_get(url):
+    import gc
     import socket
     proto, _, host, path = url.split('/', 3)
     buffer = b''
     try:
         if proto=='http:':
-            addr = socket.getaddrinfo(host, 80)[0][-1]
             s = socket.socket()
-            s.connect(addr)
+            s.connect( socket.getaddrinfo(host, 80)[0][-1] )
             s.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n' % (path, host), 'utf8'))
             while True:
                 data = s.recv(100)
@@ -20,16 +20,17 @@ def http_get(url):
         elif proto=='https:':
             import ussl
             # See http://docs.micropython.org/en/v1.9.3/esp8266/library/ussl.html
-            addr = socket.getaddrinfo(host, 443)[0][-1]
             s = socket.socket()
+            s.connect( socket.getaddrinfo(host, 443)[0][-1] )
             try:
                 ss = ussl.wrap_socket(s)
             except:
                 raise('ssl_handshake_status: failed')
-            ss.connect(addr)
-            ss.send(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n' % (path, host), 'utf8'))
+            gc.collect()
+            ss.write(bytes('GET /%s HTTP/1.0\r\nHost: %s\r\nConnection: close\r\n\r\n' % (path, host), 'utf8'))
             while True:
-                data = ss.recv(100)
+                gc.collect()
+                data = ss.read()
                 if data:
                     buffer = buffer + data 
                 else:
@@ -37,10 +38,10 @@ def http_get(url):
             ss.close()
         else:
             raise('Unsupported protocol or malformed URL')
-    except exc:
+    except:
         buffer = b''
         # consider printing the exception error code or passing it up
-        sys.print_exception(exc)
+        print('Error downloading web page /%s from %s using %s' % (path, host, proto))
     finally:
         pass
     return(buffer)
@@ -48,5 +49,5 @@ def http_get(url):
 """ To test run:
 import web_client
 print( str( web_client.http_get('http://www.bradley.edu/'), 'utf8' ), end='')
-print( str( web_client.http_get('https://www.bradley.edu/'), 'utf8' ), end='')  # this fails due to unsupported SLL protocol
+# Warning! Large pages may run out of memory especially when using https
 """
